86c264e1490dc887d7a168b92748974852d39802
John <deedsoftheforsaken@gmail.com>
2024-09-18 20:56:51 -0400

    Initial commit

---
+++ app/app.go
@@ -0,0 +1,9 @@
+package app
+
+const (
+	Name      = "minno"
+	Version   = "0.0.1"
+	LongDesc  = "A CLI tool for securing and obtaining credentials"
+	ShortDesc = "A CLI tool for securing and obtaining credentials"
+	RepoUrl   = "https://github.com/engmtcdrm/minno"
+)
