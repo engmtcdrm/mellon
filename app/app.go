392bd21a4460d8922c98117bab89f5b3488f08b0
engmtcdrm <deedsoftheforsaken@gmail.com>
2024-09-26 21:20:11 -0400

    Added comments

--- app/app.go
+++ app/app.go
@@ -1,9 +1,22 @@
 package app
 
+// Application constants
 const (
-	Name      = "minno"
-	Version   = "0.0.1"
-	LongDesc  = "A CLI tool for securing and obtaining credentials"
+	// Name of the application
+	Name = "minno"
+
+	// DotName is the name of the application with a dot prefix
+	DotName = "." + Name
+
+	// Version of the application
+	Version = "0.0.1"
+
+	// LongDesc provides a detailed description of the application
+	LongDesc = "A CLI tool for securing and obtaining credentials"
+
+	// ShortDesc provides a brief description of the application
 	ShortDesc = "A CLI tool for securing and obtaining credentials"
-	RepoUrl   = "https://github.com/engmtcdrm/minno"
+
+	// RepoUrl is the URL of the application's repository
+	RepoUrl = "https://github.com/engmtcdrm/minno"
 )
