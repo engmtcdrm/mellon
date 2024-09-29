f320067cfa203d742da75731f13be53500f8e375
John <deedsoftheforsaken@gmail.com>
2024-09-29 15:18:26 -0400

    Replaced fatih/color with own package Refactored ThemeMinno func

--- utils/prettyprint/prettyprint.go
+++ utils/prettyprint/prettyprint.go
@@ -1,171 +1,191 @@
 package prettyprint
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/charmbracelet/huh"
 	"github.com/charmbracelet/lipgloss"
-	"github.com/fatih/color"
+
+	"github.com/engmtcdrm/minno/utils/ansi"
 )
 
-func IconAlert(icon string) string {
-	if icon == "" {
-		return color.YellowString("[!] ")
-	}
+var (
+	IconComplete = Green("[✓] ")
+	IconAlert    = Yellow("[!] ")
+	IconFailed   = Red("[✗] ")
+	IconInfo     = Cyan("[i] ")
+)
 
-	return color.YellowString(icon)
+// Black formats using the default formats for its operands and returns the
+// resulting string with a black foreground. Spaces are added between
+// operands when neither is a string.
+func Black(a ...any) string {
+	return ansi.Black + fmt.Sprint(a...) + ansi.Reset
 }
 
-func IconComplete(icon string) string {
-	if icon == "" {
-		return color.GreenString("[\u2713] ")
-	}
-
-	return color.GreenString(icon)
+// Blackf formats according to a format specifier and returns the resulting
+// string with a black foreground.
+func Blackf(format string, a ...any) string {
+	return ansi.Black + fmt.Sprintf(format, a...) + ansi.Reset
 }
 
-func IconInfo(icon string) string {
-	if icon == "" {
-		return color.CyanString("[i] ")
-	}
-
-	return color.CyanString(icon)
+// Red formats using the default formats for its operands and returns the
+// resulting string with a red foreground. Spaces are added between
+// operands when neither is a string.
+func Red(a ...any) string {
+	return ansi.Red + fmt.Sprint(a...) + ansi.Reset
 }
 
-func IconFailed(icon string) string {
-	if icon == "" {
-		return color.RedString("[\u2717] ")
-	}
-
-	return color.RedString(icon)
+// Redf formats according to a format specifier and returns the resulting
+// string with a red foreground.
+func Redf(format string, a ...any) string {
+	return ansi.Red + fmt.Sprintf(format, a...) + ansi.Reset
 }
 
-func Complete(msg string, icon ...string) string {
-	_icon := ""
-
-	if len(icon) > 0 {
-		_icon = icon[0]
-	}
-
-	return IconComplete(_icon) + msg
+// Green formats using the default formats for its operands and returns the
+// resulting string with a green foreground. Spaces are added between
+// operands when neither is a string.
+func Green(a ...any) string {
+	return ansi.Green + fmt.Sprint(a...) + ansi.Reset
 }
 
-func Alert(msg string, icon ...string) string {
-	_icon := ""
-
-	if len(icon) > 0 {
-		_icon = icon[0]
-	}
-
-	return IconAlert(_icon) + msg
+// Greenf formats according to a format specifier and returns the resulting
+// string with a green foreground.
+func Greenf(format string, a ...any) string {
+	return ansi.Green + fmt.Sprintf(format, a...) + ansi.Reset
 }
 
-func Fail(msg string, icon ...string) string {
-	_icon := ""
-
-	if len(icon) > 0 {
-		_icon = icon[0]
-	}
-	return IconFailed(_icon) + msg
+// Yellow formats using the default formats for its operands and returns the
+// resulting string with a yellow foreground. Spaces are added between
+// operands when neither is a string.
+func Yellow(a ...any) string {
+	return ansi.Yellow + fmt.Sprint(a...) + ansi.Reset
 }
 
-func Info(msg string, icon ...string) string {
-	_icon := ""
+// Yellowf formats according to a format specifier and returns the resulting
+// string with a yellow foreground.
+func Yellowf(format string, a ...any) string {
+	return ansi.Yellow + fmt.Sprintf(format, a...) + ansi.Reset
+}
 
-	if len(icon) > 0 {
-		_icon = icon[0]
-	}
+// Blue formats using the default formats for its operands and returns the
+// resulting string with a blue foreground. Spaces are added between
+// operands when neither is a string.
+func Blue(a ...any) string {
+	return ansi.Blue + fmt.Sprint(a...) + ansi.Reset
+}
 
-	return IconInfo(_icon) + msg
+// Bluef formats according to a format specifier and returns the resulting
+// string with a blue foreground.
+func Bluef(format string, a ...any) string {
+	return ansi.Blue + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
+// Magenta formats using the default formats for its operands and returns the
+// resulting string with a magenta foreground. Spaces are added between
+// operands when neither is a string.
+func Magenta(a ...any) string {
+	return ansi.Magenta + fmt.Sprint(a...) + ansi.Reset
+}
+
+// Magentaf formats according to a format specifier and returns the resulting
+// string with a magenta foreground.
+func Magentaf(format string, a ...any) string {
+	return ansi.Magenta + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
+// Cyan formats using the default formats for its operands and returns the
+// resulting string with a cyan foreground. Spaces are added between
+// operands when neither is a string.
+func Cyan(a ...any) string {
+	return ansi.Cyan + fmt.Sprint(a...) + ansi.Reset
+}
+
+// Cyanf formats according to a format specifier and returns the resulting
+// string with a cyan foreground.
+func Cyanf(format string, a ...any) string {
+	return ansi.Cyan + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
+// White formats using the default formats for its operands and returns the
+// resulting string with a white foreground. Spaces are added between
+// operands when neither is a string.
+func White(a ...any) string {
+	return ansi.White + fmt.Sprint(a...) + ansi.Reset
+}
+
+// Whitef formats according to a format specifier and returns the resulting
+// string with a white foreground.
+func Whitef(format string, a ...any) string {
+	return ansi.White + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
+func Complete(msg string) string {
+	return IconComplete + msg
+}
+
+func Alert(msg string) string {
+	return IconAlert + msg
+}
+
+func Fail(msg string) string {
+	return IconFailed + msg
+}
+
+func Info(msg string) string {
+	return IconInfo + msg
 }
 
 func Var(variable string, value string) string {
-	return Info(color.CyanString(variable) + " is set to " + color.GreenString(value))
+	return Info(Cyan(variable) + " is set to " + Green(value))
 }
 
 func ThemeMinno() *huh.Theme {
 	t := huh.ThemeBase()
 
-	t.Focused.Base = lipgloss.NewStyle().PaddingLeft(1).BorderStyle(lipgloss.ThickBorder()).BorderLeft(true)
-
-	// GBNT
-	// var (
-	// 	background = lipgloss.AdaptiveColor{Dark: "#314056"}
-	// 	selection  = lipgloss.AdaptiveColor{Dark: "#435775"}
-	// 	foreground = lipgloss.AdaptiveColor{Dark: "#f8f8f2"}
-	// 	comment    = lipgloss.AdaptiveColor{Dark: "#747572"}
-	// 	green      = lipgloss.AdaptiveColor{Dark: "#B5D2AD"}
-	// 	purple     = lipgloss.AdaptiveColor{Dark: "#196589"}
-	// 	red        = lipgloss.AdaptiveColor{Dark: "#C5283D"}
-	// 	// red        = lipgloss.AdaptiveColor{Dark: "#F51000"}
-	// 	yellow = lipgloss.AdaptiveColor{Dark: "#00B0A5"}
-	// )
-
-	// DWARF
-	// var (
-	// 	background = lipgloss.AdaptiveColor{Dark: "#414342"}
-	// 	selection  = lipgloss.AdaptiveColor{Dark: "#5A5E5C"}
-	// 	foreground = lipgloss.AdaptiveColor{Dark: "#E7E4DB"}
-	// 	comment    = lipgloss.AdaptiveColor{Dark: "#968D8E"}
-	// 	green      = lipgloss.AdaptiveColor{Dark: "#7BF9FF"}
-	// 	purple     = lipgloss.AdaptiveColor{Dark: "#9573FF"}
-	// 	red        = lipgloss.AdaptiveColor{Dark: "#BF4A40"}
-	// 	yellow     = lipgloss.AdaptiveColor{Dark: "#EE9739"}
-	// )
-
-	// BASIC
 	var (
-		background = lipgloss.AdaptiveColor{Dark: "#414342"}
-		selection  = lipgloss.AdaptiveColor{Dark: "#5A5E5C"}
-		foreground = lipgloss.AdaptiveColor{Dark: "#E7E4DB"}
-		comment    = lipgloss.AdaptiveColor{Dark: "#968D8E"}
-		green      = lipgloss.AdaptiveColor{Dark: "#7BF9FF"}
-		purple     = lipgloss.AdaptiveColor{Dark: "#9573FF"}
-		red        = lipgloss.AdaptiveColor{Dark: "#BF4A40"}
-		// yellow     = lipgloss.AdaptiveColor{Dark: "#EE9739"}
+		black       = lipgloss.Color(strconv.Itoa(ansi.ANSIBlack))
+		green       = lipgloss.Color(strconv.Itoa(ansi.ANSIGreen))
+		yellow      = lipgloss.Color(strconv.Itoa(ansi.ANSIYellow))
+		magenta     = lipgloss.Color(strconv.Itoa(ansi.ANSIMagenta))
+		cyan        = lipgloss.Color(strconv.Itoa(ansi.ANSICyan))
+		white       = lipgloss.Color(strconv.Itoa(ansi.ANSIWhite))
+		blackBright = lipgloss.Color(strconv.Itoa(ansi.ANSIBrightBlack))
+		red         = lipgloss.Color(strconv.Itoa(ansi.ANSIBrightRed))
 	)
 
-	yellow2 := lipgloss.Color("0") // gray, unset
-	yellow2 = lipgloss.Color("1")  // red
-	yellow2 = lipgloss.Color("2")  // green
-	yellow2 = lipgloss.Color("3")  // yellow
-	yellow2 = lipgloss.Color("4")  // blue
-	yellow2 = lipgloss.Color("5")  // magenta
-	yellow2 = lipgloss.Color("6")  // cyan
-	yellow2 = lipgloss.Color("7")  // white
-	yellow2 = lipgloss.Color("8")  // gray?
-	yellow2 = lipgloss.Color("9")  // red highlight
-	// yellow2 = lipgloss.Color("10") // green highlight
-	// yellow2 = lipgloss.Color("11") // yellow highlight
-	// yellow2 = lipgloss.Color("12") // blue highlight
-	yellow2 = lipgloss.Color("13") // magenta highlight
-	// yellow2 = lipgloss.Color("14") // cyan highlight
-	// yellow2 = lipgloss.Color("15") // white highlight
-
-	t.Focused.Base = t.Focused.Base.BorderForeground(selection)
-	t.Focused.Title = t.Focused.Title.Foreground(purple)
-	t.Focused.NoteTitle = t.Focused.NoteTitle.Foreground(purple)
-	t.Focused.Description = t.Focused.Description.Foreground(comment)
+	t.Focused.Base = t.Focused.Base.BorderForeground(yellow)
+	t.Focused.Title = t.Focused.Title.Foreground(cyan)
+	t.Focused.NoteTitle = t.Focused.NoteTitle.Foreground(cyan)
+	t.Focused.Directory = t.Focused.Directory.Foreground(cyan)
+	t.Focused.Description = t.Focused.Description.Foreground(blackBright)
 	t.Focused.ErrorIndicator = t.Focused.ErrorIndicator.Foreground(red)
-	t.Focused.Directory = t.Focused.Directory.Foreground(purple)
-	t.Focused.File = t.Focused.File.Foreground(foreground)
 	t.Focused.ErrorMessage = t.Focused.ErrorMessage.Foreground(red)
-	t.Focused.SelectSelector = t.Focused.SelectSelector.Foreground(yellow2)
-	t.Focused.NextIndicator = t.Focused.NextIndicator.Foreground(yellow2)
-	t.Focused.PrevIndicator = t.Focused.PrevIndicator.Foreground(yellow2)
-	t.Focused.Option = t.Focused.Option.Foreground(foreground)
-	t.Focused.MultiSelectSelector = t.Focused.MultiSelectSelector.Foreground(yellow2)
+	t.Focused.SelectSelector = t.Focused.SelectSelector.Foreground(yellow)
+	t.Focused.NextIndicator = t.Focused.NextIndicator.Foreground(yellow)
+	t.Focused.PrevIndicator = t.Focused.PrevIndicator.Foreground(yellow)
+	t.Focused.Option = t.Focused.Option.Foreground(white)
+	t.Focused.MultiSelectSelector = t.Focused.MultiSelectSelector.Foreground(yellow)
 	t.Focused.SelectedOption = t.Focused.SelectedOption.Foreground(green)
-	t.Focused.SelectedPrefix = t.Focused.SelectedPrefix.Foreground(green)
-	t.Focused.UnselectedOption = t.Focused.UnselectedOption.Foreground(foreground)
-	t.Focused.UnselectedPrefix = t.Focused.UnselectedPrefix.Foreground(comment)
-	t.Focused.FocusedButton = t.Focused.FocusedButton.Foreground(yellow2).Background(purple).Bold(true)
-	t.Focused.BlurredButton = t.Focused.BlurredButton.Foreground(foreground).Background(background)
+	t.Focused.UnselectedOption = t.Focused.UnselectedOption.Foreground(white)
+	t.Focused.SelectedPrefix = t.Focused.SelectedPrefix.Foreground(green).SetString("✓ ")
+	t.Focused.UnselectedPrefix = t.Focused.UnselectedPrefix.Foreground(white).SetString("• ")
+	t.Focused.FocusedButton = t.Focused.FocusedButton.Foreground(white).Background(magenta)
+	t.Focused.BlurredButton = t.Focused.BlurredButton.Foreground(white).Background(black)
 
-	t.Focused.TextInput.Cursor = t.Focused.TextInput.Cursor.Foreground(yellow2)
-	t.Focused.TextInput.Placeholder = t.Focused.TextInput.Placeholder.Foreground(comment)
-	t.Focused.TextInput.Prompt = t.Focused.TextInput.Prompt.Foreground(yellow2)
+	t.Focused.TextInput.Cursor = t.Focused.TextInput.Cursor.Foreground(red)
+	t.Focused.TextInput.Placeholder = t.Focused.TextInput.Placeholder.Foreground(blackBright)
+	t.Focused.TextInput.Prompt = t.Focused.TextInput.Prompt.Foreground(yellow)
 
 	t.Blurred = t.Focused
 	t.Blurred.Base = t.Blurred.Base.BorderStyle(lipgloss.HiddenBorder())
+	t.Blurred.NoteTitle = t.Blurred.NoteTitle.Foreground(blackBright)
+	t.Blurred.Title = t.Blurred.NoteTitle.Foreground(blackBright)
+
+	t.Blurred.TextInput.Prompt = t.Blurred.TextInput.Prompt.Foreground(blackBright)
+	t.Blurred.TextInput.Text = t.Blurred.TextInput.Text.Foreground(white)
+
 	t.Blurred.NextIndicator = lipgloss.NewStyle()
 	t.Blurred.PrevIndicator = lipgloss.NewStyle()
 
