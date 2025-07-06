5f9213cc2409cc21177b30a258482ca09e50bdfb
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-07-06 16:04:42 -0400

    Simplified to just display list of available secrets

--- cmd/list.go
+++ cmd/list.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/engmtcdrm/minno/app"
-	"github.com/engmtcdrm/minno/header"
 	"github.com/spf13/cobra"
 )
 
@@ -12,29 +11,14 @@ func init() {
 	rootCmd.AddCommand(listCmd)
 }
 
-// func validateListFlags(cmd *cobra.Command, args []string) error {
-// 	return nil
-// }
-
 var listCmd = &cobra.Command{
 	Use:     "list",
 	Short:   "List available secrets",
 	Long:    "List available secrets",
 	Example: fmt.Sprintf("  %s list", app.Name),
-	// PreRunE: validateListFlags,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		header.PrintHeader()
-
-		if len(secretFiles) == 0 {
-			fmt.Println("No secrets found.")
-
-			return nil
-		}
-
-		fmt.Printf("%d available secret(s):\n\n", len(secretFiles))
-
 		for _, secret := range secretFiles {
-			fmt.Printf("- %s\n", secret.Name)
+			fmt.Println(secret.Name)
 		}
 
 		return nil
