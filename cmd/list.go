bb61a8451e40d178f1badaba3f348ca1c3c1a6f5
John <deedsoftheforsaken@gmail.com>
2025-07-09 17:08:45 -0400

    Added print flag Added prettier logging when not using print flag

--- cmd/list.go
+++ cmd/list.go
@@ -3,11 +3,21 @@ package cmd
 import (
 	"fmt"
 
+	pp "github.com/engmtcdrm/go-prettyprint"
 	"github.com/engmtcdrm/minno/app"
+	"github.com/engmtcdrm/minno/header"
 	"github.com/spf13/cobra"
 )
 
 func init() {
+	listCmd.Flags().BoolVarP(
+		&print,
+		"print",
+		"p",
+		false,
+		"(optional) Whether to print only the names of the secrets without additional information. Defaults to false.",
+	)
+
 	rootCmd.AddCommand(listCmd)
 }
 
@@ -17,8 +27,26 @@ var listCmd = &cobra.Command{
 	Long:    "List available secrets",
 	Example: fmt.Sprintf("  %s list", app.Name),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if print {
+			for _, secret := range secretFiles {
+				fmt.Println(secret.Name)
+			}
+
+			return nil
+		}
+
+		header.PrintHeader()
+
+		if len(secretFiles) == 0 {
+			fmt.Printf("No available secrets. Use command %s to create a secret\n", pp.Greenf("%s create", envVars.ExeCmd))
+
+			return nil
+		}
+
+		fmt.Println(pp.Cyan("Available Secrets"))
+		fmt.Println("")
 		for _, secret := range secretFiles {
-			fmt.Println(secret.Name)
+			fmt.Printf("%s\n", pp.Green(secret.Name))
 		}
 
 		return nil
