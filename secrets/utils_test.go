c32c2ebb8629c483d32cb7958d207ca55d5b15e6
John <deedsoftheforsaken@gmail.com>
2026-01-31 10:47:44 -0500

    chore: more test coverage

--- secrets/utils_test.go
+++ secrets/utils_test.go
@@ -1,10 +1,50 @@
 package secrets
 
 import (
+	"os"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestGetSecretFiles(t *testing.T) {
+	keyPath := path.Join(t.TempDir(), "test.key")
+
+	secretExt := ".secret"
+
+	createSecretDir := func() string {
+		dirPath := path.Join(t.TempDir(), "secrets")
+		err := os.MkdirAll(dirPath, 0755)
+		assert.NoError(t, err)
+		return dirPath
+	}
+
+	t.Run("empty keyPath", func(t *testing.T) {
+		secretsPath := createSecretDir()
+		defer os.RemoveAll(secretsPath)
+
+		emptySecrets, err := GetSecretFiles("", secretsPath, secretExt)
+		assert.NoError(t, err)
+		assert.Empty(t, emptySecrets)
+	})
+
+	t.Run("empty secretsPath", func(t *testing.T) {
+		emptySecrets, err := GetSecretFiles(keyPath, "", secretExt)
+		assert.Error(t, err)
+		assert.Empty(t, emptySecrets)
+	})
+
+	t.Run("empty secretExt", func(t *testing.T) {
+		secretsPath := createSecretDir()
+		defer os.RemoveAll(secretsPath)
+
+		emptySecrets, err := GetSecretFiles(keyPath, secretsPath, "")
+		assert.NoError(t, err)
+		assert.Empty(t, emptySecrets)
+	})
+
+}
 
 func TestValidateName(t *testing.T) {
 	// Valid names
